abd03d5b16b44c0eed51b0aa1252445299e88d0f
Hake Huang <hake.huang@nxp.com>
2026-06-16 15:56:11 +0800

    app: move in adc power measure sample from zephyr PR 105807
    Signed-off-by: Hake Huang <hake.huang@nxp.com>

--- README.md
+++ README.md
@@ -1,138 +1,33 @@
-# Zephyr Example Application
+# Power Shield ADC Sample
 
-<a href="https://github.com/zephyrproject-rtos/example-application/actions/workflows/build.yml?query=branch%3Amain">
-  <img src="https://github.com/zephyrproject-rtos/example-application/actions/workflows/build.yml/badge.svg?event=push">
-</a>
-<a href="https://github.com/zephyrproject-rtos/example-application/actions/workflows/docs.yml?query=branch%3Amain">
-  <img src="https://github.com/zephyrproject-rtos/example-application/actions/workflows/docs.yml/badge.svg?event=push">
-</a>
-<a href="https://zephyrproject-rtos.github.io/example-application">
-  <img alt="Documentation" src="https://img.shields.io/badge/documentation-3D578C?logo=sphinx&logoColor=white">
-</a>
-<a href="https://zephyrproject-rtos.github.io/example-application/doxygen">
-  <img alt="API Documentation" src="https://img.shields.io/badge/API-documentation-3D578C?logo=c&logoColor=white">
-</a>
+This repository now carries the Zephyr ADC power measurement sample from
+`zephyrproject-rtos/zephyr` PR #105807, adapted to live in this repository's
+`app/` directory.
 
-This repository contains a Zephyr example application. The main purpose of this
-repository is to serve as a reference on how to structure Zephyr-based
-applications. Some of the features demonstrated in this example are:
+## What is included
 
-- Basic [Zephyr application][app_dev] skeleton
-- [Zephyr workspace applications][workspace_app]
-- [Zephyr modules][modules]
-- [West T2 topology][west_t2]
-- [Custom boards][board_porting]
-- Custom [devicetree bindings][bindings]
-- Out-of-tree [drivers][drivers]
-- Out-of-tree libraries
-- Example CI configuration (using GitHub Actions)
-- Custom [west extension][west_ext]
-- Custom [Zephyr runner][runner_ext]
-- Doxygen and Sphinx documentation boilerplate
+- ADC power measurement application in `app/`
+- Board overlays/config fragments for:
+  - `frdm_k64f`
+  - `frdm_mcxc242`
+  - `frdm_mcxc444`
+  - `mimxrt1040_evk`
+  - `mimxrt595_evk_cm33`
+- Sample documentation in `app/README.rst`
+- Twister metadata in `app/sample.yaml`
 
-This repository is versioned together with the [Zephyr main tree][zephyr]. This
-means that every time that Zephyr is tagged, this repository is tagged as well
-with the same version number, and the [manifest](west.yml) entry for `zephyr`
-will point to the corresponding Zephyr tag. For example, the `example-application`
-v2.6.0 will point to Zephyr v2.6.0. Note that the `main` branch always
-points to the development branch of Zephyr, also `main`.
+## Build
 
-[app_dev]: https://docs.zephyrproject.org/latest/develop/application/index.html
-[workspace_app]: https://docs.zephyrproject.org/latest/develop/application/index.html#zephyr-workspace-app
-[modules]: https://docs.zephyrproject.org/latest/develop/modules.html
-[west_t2]: https://docs.zephyrproject.org/latest/develop/west/workspaces.html#west-t2
-[board_porting]: https://docs.zephyrproject.org/latest/guides/porting/board_porting.html
-[bindings]: https://docs.zephyrproject.org/latest/guides/dts/bindings.html
-[drivers]: https://docs.zephyrproject.org/latest/reference/drivers/index.html
-[zephyr]: https://github.com/zephyrproject-rtos/zephyr
-[west_ext]: https://docs.zephyrproject.org/latest/develop/west/extensions.html
-[runner_ext]: https://docs.zephyrproject.org/latest/develop/modules.html#external-runners
+From a Zephyr workspace with this repository available as the application:
 
-## Getting Started
-
-Before getting started, make sure you have a proper Zephyr development
-environment. Follow the official
-[Zephyr Getting Started Guide](https://docs.zephyrproject.org/latest/getting_started/index.html).
-
-### Initialization
-
-The first step is to initialize the workspace folder (``my-workspace``) where
-the ``example-application`` and all Zephyr modules will be cloned. Run the following
-command:
-
-```shell
-# initialize my-workspace for the example-application (main branch)
-west init -m https://github.com/zephyrproject-rtos/example-application --mr main my-workspace
-# update Zephyr modules
-cd my-workspace
-west update
+```sh
+west build -b frdm_mcxc444/mcxc444 app
 ```
 
-### Building and running
+To try another supported board, replace the board name with one of the boards
+covered by the overlay/config files in `app/boards`.
 
-To build the application, run the following command:
+## Notes
 
-```shell
-cd example-application
-west build -b $BOARD app
-```
-
-where `$BOARD` is the target board.
-
-You can use the `custom_plank` board found in this
-repository. Note that Zephyr sample boards may be used if an
-appropriate overlay is provided (see `app/boards`).
-
-A sample debug configuration is also provided. To apply it, run the following
-command:
-
-```shell
-west build -b $BOARD app -- -DEXTRA_CONF_FILE=debug.conf
-```
-
-Once you have built the application, run the following command to flash it:
-
-```shell
-west flash
-```
-
-### Testing
-
-To execute Twister integration tests, run the following command:
-
-```shell
-west twister -T tests --integration
-```
-
-### Documentation
-
-A minimal documentation setup is provided for Doxygen and Sphinx. To build the
-documentation first change to the ``doc`` folder:
-
-```shell
-cd doc
-```
-
-Before continuing, check if you have Doxygen installed. It is recommended to
-use the same Doxygen version used in [CI](.github/workflows/docs.yml). To
-install Sphinx, make sure you have a Python installation in place and run:
-
-```shell
-pip install -r requirements.txt
-```
-
-API documentation (Doxygen) can be built using the following command:
-
-```shell
-doxygen
-```
-
-The output will be stored in the ``_build_doxygen`` folder. Similarly, the
-Sphinx documentation (HTML) can be built using the following command:
-
-```shell
-make html
-```
-
-The output will be stored in the ``_build_sphinx`` folder. You may check for
-other output formats other than HTML by running ``make help``.
+The application content was moved from the upstream Zephyr sample path:
+`samples/drivers/adc/adc_power_measure`.
--- app/src/main.c
+++ app/src/main.c
@@ -1,80 +1,231 @@
 /*
- * Copyright (c) 2021 Nordic Semiconductor ASA
+ * Copyright (c) 2024 Centro de Inovacao EDGE
+ * Copyright 2025 NXP
+ *
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <zephyr/drivers/adc.h>
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/sensor.h>
-#include <zephyr/logging/log.h>
+#include <zephyr/shell/shell.h>
+#include <zephyr/drivers/uart.h>
 
-#include <app/drivers/blink.h>
+/* ADC node from the devicetree. */
+#define ADC_NODE DT_ALIAS(adc0)
 
-#include <app_version.h>
+/* Auxiliary macro to obtain channel vref, if available. */
+#define CHANNEL_VREF(node_id) DT_PROP_OR(node_id, zephyr_vref_mv, 0)
 
-LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
+/* Data of ADC device specified in devicetree. */
+static const struct device *adc = DEVICE_DT_GET(ADC_NODE);
 
-#define BLINK_PERIOD_MS_STEP 100U
-#define BLINK_PERIOD_MS_MAX  1000U
+/* Data array of ADC channels for the specified ADC. */
+static const struct adc_channel_cfg channel_cfgs[] = {
+	DT_FOREACH_CHILD_SEP(ADC_NODE, ADC_CHANNEL_CFG_DT, (,))};
 
-int main(void)
+/* Data array of ADC channel voltage references. */
+static uint32_t vrefs_mv[] = {DT_FOREACH_CHILD_SEP(ADC_NODE, CHANNEL_VREF, (,))};
+
+/* Get the number of channels defined on the DTS. */
+#define CHANNEL_COUNT ARRAY_SIZE(channel_cfgs)
+
+#ifdef CONFIG_SEQUENCE_32BITS_REGISTERS
+static uint32_t channel_reading[CONFIG_SEQUENCE_SAMPLES][CHANNEL_COUNT];
+#else
+static uint16_t channel_reading[CONFIG_SEQUENCE_SAMPLES][CHANNEL_COUNT];
+#endif
+
+/* Options for the sequence sampling. */
+static const struct adc_sequence_options options = {
+	.extra_samplings = CONFIG_SEQUENCE_SAMPLES - 1,
+	.interval_us = 0,
+};
+
+/* Configure the sampling sequence to be made. */
+static struct adc_sequence sequence = {
+	.buffer = channel_reading,
+	/* buffer size in bytes, not number of samples */
+	.buffer_size = sizeof(channel_reading),
+	.resolution = CONFIG_SEQUENCE_RESOLUTION,
+	.options = &options,
+};
+
+static uint32_t count;
+
+#ifndef CONFIG_SHELL_ADC_PS
+static const struct device *const dev_console = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+#endif
+
+#ifdef CONFIG_SHELL_ADC_PS
+/* ADC shell command handlers */
+static int cmd_adc_read(const struct shell *sh, size_t argc, char **argv)
 {
-	int ret;
-	unsigned int period_ms = BLINK_PERIOD_MS_MAX;
-	const struct device *sensor, *blink;
-	struct sensor_value last_val = { 0 }, val;
+	int err = 0;
 
-	printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
+	ARG_UNUSED(argc);
+	ARG_UNUSED(argv);
 
-	sensor = DEVICE_DT_GET(DT_NODELABEL(example_sensor));
-	if (!device_is_ready(sensor)) {
-		LOG_ERR("Sensor not ready");
-		return 0;
+	shell_print(sh, "ADC sequence reading [%u]:", count++);
+
+	err = adc_read(adc, &sequence);
+	if (err < 0) {
+		shell_print(sh, "Could not read (%d)", err);
+		return -EIO;
 	}
 
-	blink = DEVICE_DT_GET(DT_NODELABEL(blink_led));
-	if (!device_is_ready(blink)) {
-		LOG_ERR("Blink LED not ready");
-		return 0;
-	}
+	for (size_t channel_index = 0U; channel_index < CHANNEL_COUNT; channel_index++) {
+		int32_t val_mv;
+		int32_t val_raw;
 
-	ret = blink_off(blink);
-	if (ret < 0) {
-		LOG_ERR("Could not turn off LED (%d)", ret);
-		return 0;
-	}
+		shell_print(sh,
+			    "- %s, channel %" PRId32 ", %" PRId32 " sequence samples (diff = %d):",
+			    adc->name, channel_cfgs[channel_index].channel_id,
+			    CONFIG_SEQUENCE_SAMPLES, channel_cfgs[channel_index].differential);
+		for (size_t sample_index = 0U; sample_index < CONFIG_SEQUENCE_SAMPLES;
+		     sample_index++) {
+			val_raw = channel_reading[sample_index][channel_index];
+			val_mv = val_raw;
+			err = adc_raw_to_millivolts(vrefs_mv[channel_index],
+						    channel_cfgs[channel_index].gain,
+						    CONFIG_SEQUENCE_RESOLUTION, &val_mv);
 
-	printk("Use the sensor to change LED blinking period\n");
-
-	while (1) {
-		ret = sensor_sample_fetch(sensor);
-		if (ret < 0) {
-			LOG_ERR("Could not fetch sample (%d)", ret);
-			return 0;
-		}
-
-		ret = sensor_channel_get(sensor, SENSOR_CHAN_PROX, &val);
-		if (ret < 0) {
-			LOG_ERR("Could not get sample (%d)", ret);
-			return 0;
-		}
-
-		if ((last_val.val1 == 0) && (val.val1 == 1)) {
-			if (period_ms == 0U) {
-				period_ms = BLINK_PERIOD_MS_MAX;
+			/* conversion to mV may not be supported, skip if not */
+			if ((err < 0) || vrefs_mv[channel_index] == 0) {
+				shell_print(sh, "- - %" PRId32 " (value in mV not available)",
+					    val_raw);
 			} else {
-				period_ms -= BLINK_PERIOD_MS_STEP;
+				shell_print(sh, "- - %" PRId32 " = %" PRId32 "mV", val_raw, val_mv);
 			}
-
-			printk("Proximity detected, setting LED period to %u ms\n",
-			       period_ms);
-			blink_set_period_ms(blink, period_ms);
 		}
-
-		last_val = val;
-
-		k_sleep(K_MSEC(100));
 	}
+	shell_print(sh, "==== end of reading ===");
 
 	return 0;
 }
 
+static int cmd_adc_status(const struct shell *sh, size_t argc, char **argv)
+{
+	ARG_UNUSED(argc);
+	ARG_UNUSED(argv);
+
+	shell_print(sh, "%s", "\n");
+	shell_print(sh, "==== start of adc features ===");
+	shell_print(sh, "CHANNEL_COUNT: %d", CHANNEL_COUNT);
+	shell_print(sh, "Resolution: %d", CONFIG_SEQUENCE_RESOLUTION);
+	for (size_t channel_index = 0U; channel_index < CHANNEL_COUNT; channel_index++) {
+		shell_print(sh,
+			    "\tchannel_id %d features:", channel_cfgs[channel_index].channel_id);
+		if (channel_cfgs[channel_index].differential) {
+			shell_print(sh, "\t - is diff mode");
+		} else {
+			shell_print(sh, "\t - is single mode");
+		}
+		shell_print(sh, "\t - verf is %d mv", vrefs_mv[channel_index]);
+	}
+	shell_print(sh, "==== end of adc features ===");
+
+	return 0;
+}
+
+/* ADC subcommands */
+SHELL_STATIC_SUBCMD_SET_CREATE(adc_cmds,
+			       SHELL_CMD_ARG(read, NULL, "Read ADC values", cmd_adc_read, 1, 0),
+			       SHELL_CMD_ARG(status, NULL, "Show ADC status", cmd_adc_status, 1, 0),
+			       SHELL_SUBCMD_SET_END);
+
+/* Register main ADC command */
+SHELL_CMD_REGISTER(adc, &adc_cmds, "ADC power shield commands", NULL);
+
+#endif
+
+int main(void)
+{
+	int err;
+
+	if (!device_is_ready(adc)) {
+		return 0;
+	}
+
+	/* Configure channels individually prior to sampling. */
+	for (size_t i = 0U; i < CHANNEL_COUNT; i++) {
+		sequence.channels |= BIT(channel_cfgs[i].channel_id);
+		err = adc_channel_setup(adc, &channel_cfgs[i]);
+		if (err < 0) {
+			return 0;
+		}
+		if ((vrefs_mv[i] == 0) && (channel_cfgs[i].reference == ADC_REF_INTERNAL)) {
+			vrefs_mv[i] = adc_ref_internal(adc);
+		}
+	}
+	while (1) {
+#ifndef CONFIG_SHELL_ADC_PS
+		uint8_t c = 'A';
+
+		err = uart_poll_in(dev_console, &c);
+		if (err < 0) {
+			k_msleep(100);
+			continue;
+		}
+		k_msleep(100);
+
+		/* print capacity when input is '\r'*/
+		if (c == '\r') {
+			printf("\r\n==== start of adc features ===\n");
+			printf("CHANNEL_COUNT: %d\n", CHANNEL_COUNT);
+			printf("Resolution: %d\n", CONFIG_SEQUENCE_RESOLUTION);
+			for (size_t channel_index = 0U; channel_index < CHANNEL_COUNT;
+			     channel_index++) {
+				printf("\tchannel_id %d features:\n",
+				       channel_cfgs[channel_index].channel_id);
+				if (channel_cfgs[channel_index].differential) {
+					printf("\t - is diff mode\n");
+				} else {
+					printf("\t - is single mode\n");
+				}
+				printf("\t - verf is %d mv\n", vrefs_mv[channel_index]);
+			}
+			printf("==== end of adc features ===\n");
+			continue;
+		}
+
+		printf("ADC sequence reading [%u]:\n", count++);
+
+		err = adc_read(adc, &sequence);
+		if (err < 0) {
+			printf("Could not read (%d)\n", err);
+			continue;
+		}
+
+		for (size_t channel_index = 0U; channel_index < CHANNEL_COUNT; channel_index++) {
+			int32_t val_mv;
+
+			printf("- %s, channel %" PRId32 ", %" PRId32
+			       " sequence samples (diff = %d):\n",
+			       adc->name, channel_cfgs[channel_index].channel_id,
+			       CONFIG_SEQUENCE_SAMPLES, channel_cfgs[channel_index].differential);
+			for (size_t sample_index = 0U; sample_index < CONFIG_SEQUENCE_SAMPLES;
+			     sample_index++) {
+
+				val_mv = channel_reading[sample_index][channel_index];
+
+				printf("- - %" PRId32, val_mv);
+				err = adc_raw_to_millivolts(vrefs_mv[channel_index],
+							    channel_cfgs[channel_index].gain,
+							    CONFIG_SEQUENCE_RESOLUTION, &val_mv);
+
+				/* conversion to mV may not be supported, skip if not */
+				if ((err < 0) || vrefs_mv[channel_index] == 0) {
+					continue;
+				} else {
+					printf(" = %" PRId32 "mV\n", val_mv);
+				}
+			}
+		}
+		printf("==== end of reading ===\n");
+#else
+		k_msleep(100);
+#endif
+	}
+
+	return 0;
+}
